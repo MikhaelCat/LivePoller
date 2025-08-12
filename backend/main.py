35a2b26bedcb8c1dd23f22e88ba67d34e7fd9ef6
Mikhael <opanasukmihail80@gmail.com>
2025-08-12 20:10:28 +1000

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,34 +1,27 @@
 from contextlib import asynccontextmanager
-from fastapi import FastAPI  
-from fastapi.responses import JSONResponse  
+
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
 import sentry_sdk
+
 from backend.core.cache import redis_client
 from backend.services.search_service import es
 
-from datetime import datetime, timedelta
-from passlib.context import CryptContext
-
-from fastapi import FastAPI, Depends, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
-
-import jwt
-
-
-app = FastAPI(title="Auth Service", port=8001)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("🚀 Service started")
     yield
-    # Остановка
     await redis_client.close()
     await es.close()
     print("🛑 Service stopped")
 
+
 app = FastAPI(lifespan=lifespan)
+
 
 @app.exception_handler(500)
 async def internal_exception_handler(request, exc):
     sentry_sdk.capture_exception(exc)
     return JSONResponse(status_code=500, content={"detail": "Internal error"})
-

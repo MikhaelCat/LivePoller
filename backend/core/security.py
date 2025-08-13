a1b88d71b8c18ad8221f04d94c2f37f2b1cb015c
Mikhael <opanasukmihail80@gmail.com>
2025-08-13 17:31:44 +1000

    Update security.py

--- backend/core/security.py
+++ backend/core/security.py
@@ -1,16 +1,22 @@
 from datetime import datetime, timedelta
-from passlib.context import CryptContext
 
 from fastapi.security import OAuth2PasswordBearer
+from passlib.context import CryptContext
 
 import jwt
 
 
+# Настройки
 SECRET_KEY = "your-super-secret-key"
+ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+# Экземпляры
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
-def create_access_token( dict):
+
+def create_access_token(data: dict) -> str:
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})

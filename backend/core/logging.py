7e6709f5b48927e4e097f2d159eb78e60d1e1573
Mikhael <opanasukmihail80@gmail.com>
2025-08-13 18:27:12 +1000

    Update logging.py

--- backend/core/logging.py
+++ backend/core/logging.py
@@ -7,7 +7,8 @@ structlog.configure(
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.JSONRenderer()
     ],
-    wrapper_class=structlog.make_filtering_bound_logger(level=logging.INFO),
+    wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
 )
 
 logger = structlog.get_logger()
+

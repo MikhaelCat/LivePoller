90520a7c77dbb6309df95b0242d5a4cb4d5abb5c
Mikhael <opanasukmihail80@gmail.com>
2025-08-13 17:28:55 +1000

    Update security.py

--- backend/core/security.py
+++ backend/core/security.py
@@ -7,14 +7,10 @@ import jwt
 
 
 SECRET_KEY = "your-super-secret-key"
-ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
-
-def create_access_token(data: dict):
+def create_access_token( dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
